1ff15e765a0d3d3fa6a7d67d7160666ab1190b7c
noahbaxter <noahbaxt@gmail.com>
2026-09-22 19:35:38 -0700

    add stub denylist to recompiler config
    On a symbolized ELF, name binding swaps real SDK function bodies for handlers.

--- ps2xRecomp/include/ps2recomp/ps2_recompiler.h
+++ ps2xRecomp/include/ps2recomp/ps2_recompiler.h
@@ -70,6 +70,7 @@ namespace ps2recomp
         std::unordered_set<std::string> m_stubFunctions;
         std::unordered_set<uint32_t> m_stubFunctionStarts;
         std::unordered_map<uint32_t, std::string> m_stubHandlerBindingsByStart;
+        std::unordered_set<std::string> m_stubDenylist;
         std::unordered_set<uint32_t> m_entryPointHintStarts;
         std::unordered_set<uint32_t> m_correctnessCriticalFunctionStarts;
         std::map<uint32_t, std::string> m_generatedStubs;
